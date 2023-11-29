6fa0ca9a4577f8a8e053c5279021d1572b679477
Coding-Abdi <abdi.brum@gmail.com>
2023-11-29 15:05:18 +0000

    minor css changes

--- project2/airbnb/src/Card.js
+++ project2/airbnb/src/Card.js
@@ -7,10 +7,14 @@ function Card () {
     return (
         <div className="card">
             <img className="zaf-img" src={zaf} alt="Katie Zaferes wearing a trisuit and smiling"/>
-            <img className="star-img" src={star} alt="a small red star"/>
-            <p className="rating">5.0 -6- * USA</p>
+            <div className="card-stats">
+                <img className="star-img" src={star} alt="a small red star"/>
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
             <p className="description">Life lessons with Katie Zaferes</p>
-            <p className="price">From $136 / person</p>
+            <p><span className="bold">From $136</span> / person</p>
         </div>
     )
 }
