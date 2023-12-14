f670a1c4abd563c57685d77f10cb69b8b41ce344
Coding-Abdi <abdi.brum@gmail.com>
2023-12-14 14:11:44 +0000

    imported data into app component from an array

---
+++ props_exercise/props_jokes/src/jokesData.js
@@ -0,0 +1,25 @@
+const jokesData = [
+    {
+        setup: 'What is the best thing about Switzerland?',
+        punchline: 'I have no idea, but the flag is a big plus.'
+    },
+    {
+        setup: 'I invented a new word!',
+        punchline: 'Plagiarism!'
+    },
+    {
+        setup: 'Did you hear about the mathematician who is afraid of negative numbers?',
+        punchline: 'He will stop at nothing to avoid them.'
+    },
+    {
+        setup: 'Why do we tell actors to "break a leg?"',
+        punchline: 'Because every play has a cast.'
+    },
+    {
+        setup: 'Helvetica and Times New Roman walk into a bar.',
+        punchline: '"Get out of here!" shouts the bartender. "We do not serve your type."'
+    }
+]
+
+export default jokesData;
+
