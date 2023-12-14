542343e1c37e1343947c2c4fcec61df5aa9360c1
Coding-Abdi <abdi.brum@gmail.com>
2023-12-14 13:30:37 +0000

    passed props from app component to card component

--- project2/airbnb/src/App.js
+++ project2/airbnb/src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Card from './Card';
-
 import './App.css';
+let zaf = require('./assets/zaferes.png');
+let star = require('./assets/star.png');
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img={zaf}
+        starImg={star}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+       />
     </div>
   );
 }
--- project2/airbnb/src/Card.js
+++ project2/airbnb/src/Card.js
@@ -1,20 +1,20 @@
 import React from 'react';
-let zaf = require('./assets/zaferes.png');
-let star = require('./assets/star.png');
+// let zaf = require('./assets/zaferes.png');
+// let star = require('./assets/star.png');
 
 
-function Card () {
+function Card (props) {
     return (
         <div className="card">
-            <img className="zaf-img" src={zaf} alt="Katie Zaferes wearing a trisuit and smiling"/>
+            <img className="zaf-img" src={props.img} alt="Katie Zaferes wearing a trisuit and smiling"/>
             <div className="card-stats">
-                <img className="star-img" src={star} alt="a small red star"/>
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <img className="star-img" src={props.starImg} alt="a small red star"/>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray"> {props.country}</span>
             </div>
-            <p className="description">Life lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p className="description">{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
