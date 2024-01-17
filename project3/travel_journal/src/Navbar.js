8cb5432478ced0fef0706edae93e721d1dc6645b
Coding-Abdi <abdi.brum@gmail.com>
2024-01-17 13:23:19 +0000

    navbar outline created

---
+++ project3/travel_journal/src/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEarthAmerica } from '@fortawesome/free-solid-svg-icons'
+
+let logo = <FontAwesomeIcon icon={faEarthAmerica} size="2x" beat />
+
+
+
+function Navbar () {
+   
+    return (
+        <nav>
+            <h3 className="logo">{logo}</h3>
+            <h2 className="main-title">my travel journal</h2>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ project3/travel_journal/src/data.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        id: 1,
+        Title: "Mount Fuji",
+        Location: "Japan",
+        Gmaps: "5.0",
+        traveldate: "30 Jan 2024 - 10 Feb 2024",
+        description: "Mount Fuji has an elevation of 3,776m (12,389ft) and is Japan's tallest peak. It is a dormant volcano with the last eruption taking place in 1707. It is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        img: "zaferes.png"
+     }
+    ]
+
+export default data;
