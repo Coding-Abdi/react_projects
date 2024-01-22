8ff40c04fd94d544ad300c060ab22fd14c5da140
Coding-Abdi <abdi.brum@gmail.com>
2024-01-22 13:53:58 +0000

    most of the styling completed

--- project3/travel_journal/src/Card.js
+++ project3/travel_journal/src/Card.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
+let pin = <FontAwesomeIcon icon={faLocationDot} />
 
 function Card (props) {
     return (
@@ -6,11 +10,13 @@ function Card (props) {
             <div className="column">
                 <img className="card-img" src={`../assets/${props.img}`} alt="A lake with Mount Fuji in the background"/>
             </div>
-            <div className="column">
-                <h2 className="loc-maps"><span className="location">{props.location}</span><span className="g-maps"> {props.gMaps}</span></h2>
-                <p className="title">{props.title}</p>
-                <p><span>{props.startDate} -</span><span> {props.endDate}</span></p>
-                <p className="description">{props.description}</p>
+            <div className="column main-text">
+                <h2 className="loc-maps"><span>{pin} </span><span className="location">{props.location}</span><span className="g-maps"> <a href={props.gMaps} target="_blank" rel="noopener noreferrer">View on Google Maps</a></span></h2>
+                <h1 className="title">{props.title}</h1>
+                <p><span className="date">{props.startDate} -</span><span className="date"> {props.endDate}</span></p>
+                <p className="description">{props.description1}</p>
+                <p className="description">{props.description2}</p>
+                <p className="description">{props.description3}</p>
             </div>
            
             
