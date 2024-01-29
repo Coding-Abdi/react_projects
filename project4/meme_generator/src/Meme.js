3f6985341430b99884dcf9c9d4449da16b85060d
Coding-Abdi <abdi.brum@gmail.com>
2024-01-29 13:47:08 +0000

    added hardcoded data

--- project4/meme_generator/src/Meme.js
+++ project4/meme_generator/src/Meme.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import data from './data';
 
 function Meme () {
+    function getMemeImage() {
+        const memesArray = data.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url);
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input className="form-input" type="text" placeholder="Top text"/>
                 <input className="form-input" type="text" placeholder="Bottom text"/>
-                <button className="form-button">Get a new meme image 🖼 </button>
-            </form>
+                <button className="form-button" onClick={getMemeImage}>Get a new meme image 🖼 </button>
+            </div>
         </main>
     )
 }
